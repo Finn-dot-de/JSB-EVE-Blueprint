c3f73649c73d076666080c8d0d583aa88b2f15c6
Finn <finnlaskowski761@gmail.com>
2026-07-31 23:36:16 +0200

    Stable State from Auth State 4.6: Bereinigung

--- src/main/java/com/eve/own/auth/backend/domain/discord/scheduler/DiscordSyncScheduler.java
+++ src/main/java/com/eve/own/auth/backend/domain/discord/scheduler/DiscordSyncScheduler.java
@@ -30,7 +30,7 @@ public class DiscordSyncScheduler {
         this.discordBotService = discordBotService;
     }
 
-    @Scheduled(fixedRate = 600_00)
+    @Scheduled(fixedRate = 600_000)
     public void syncDiscordRoles() {
         log.info("Starte Discord Role Sync...");
         List<DiscordConnection> connections = connectionRepo.findAll();
